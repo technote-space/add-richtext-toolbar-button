a9bad3bd3335f8ece29cbf37414e1947dc929a93
technote <technote.space@gmail.com>
2019-02-17 01:55:06 +0900

    trivial change

--- src/classes/models/editor.php
+++ src/classes/models/editor.php
@@ -34,7 +34,7 @@ class Editor implements \WP_Framework_Core\Interfaces\Singleton, \WP_Framework_C
 		$handle = 'add-richtext-toolbar-button-editor';
 		$this->enqueue_style( $handle, 'gutenberg.css' );
 		$this->enqueue_script( $handle, 'add-richtext-toolbar-button-gutenberg.min.js', [
-			'wp-blocks',
+			'wp-data',
 			'wp-element',
 			'wp-rich-text',
 			'wp-components',
