2e19be3148eafcc9fffef28c1f9106ee161ad533
technote <technote.space@gmail.com>
2019-02-18 17:59:24 +0900

    trivial change

--- src/classes/models/custom_post/setting.php
+++ src/classes/models/custom_post/setting.php
@@ -722,7 +722,7 @@ class Setting implements \Richtext_Toolbar_Button\Interfaces\Models\Custom_Post 
 			'background color' => 'background-color: #9ff;',
 			'border'           => 'border: solid 2px #f9f;',
 			'border radius'    => 'border-radius: 5px;',
-			'padding'          => 'padding: 5px;',
+			'padding'          => 'padding: .5em;',
 			'shadow'           => 'box-shadow: 3px 3px 3px #ccc;',
 			'highlighter'      => 'background-image: linear-gradient(to bottom, rgba(255, 255, 255, 0) 60%, #6f6 75%);',
 			'block'            => 'display: block;',
