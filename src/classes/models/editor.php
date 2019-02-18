4697fa6f2d0c65804cb8f73dad60667a7e0624fb
technote <technote.space@gmail.com>
2019-02-18 18:30:40 +0900

    v0.0.4

--- src/classes/models/editor.php
+++ src/classes/models/editor.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * @version 0.0.3
+ * @version 0.0.4
  * @author technote-space
  * @copyright technote-space All Rights Reserved
  * @license http://www.opensource.org/licenses/gpl-2.0.php GNU General Public License, version 2
