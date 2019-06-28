6bf848d5eebfc6a8c04dc216d937aacd145f0394
Technote <technote.space@gmail.com>
2019-06-28 22:41:17 +0900

    chore: remove version info (#151)

--- src/views/admin/script/icon.php
+++ src/views/admin/script/icon.php
@@ -1,8 +1,6 @@
 <?php
 /**
- * @version 1.1.6
  * @author Technote
- * @since 1.0.0
  * @copyright Technote All Rights Reserved
  * @license http://www.opensource.org/licenses/gpl-2.0.php GNU General Public License, version 2
  * @link https://technote.space/
