ceb454829a5718ae61d3c62bcae74a92b1638491
Technote <technote.space@gmail.com>
2019-07-16 16:21:22 +0900

    update packages (#169)

---
+++ bin/packages.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+set -e
+
+packages+=( "wp-content-framework/core" )
+packages+=( "wp-content-framework/custom_post" )
+packages+=( "wp-content-framework/update" )
+packages+=( "wp-content-framework/update_check" )
+packages+=( "wp-content-framework/editor" )
